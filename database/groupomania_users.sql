ac502d363eb885e17f82399a198878c2f73f5ef2
Tom Charmasson <tomcharmasson@gmail.com>
2022-02-07 11:31:42 +0100

    Add Database

---
+++ database/groupomania_users.sql
@@ -0,0 +1,120 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+--
+-- Host: localhost    Database: groupomania
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username_2` (`username`),
+  UNIQUE KEY `email_2` (`email`),
+  UNIQUE KEY `username_3` (`username`),
+  UNIQUE KEY `email_3` (`email`),
+  UNIQUE KEY `username_4` (`username`),
+  UNIQUE KEY `email_4` (`email`),
+  UNIQUE KEY `username_5` (`username`),
+  UNIQUE KEY `email_5` (`email`),
+  UNIQUE KEY `username_6` (`username`),
+  UNIQUE KEY `email_6` (`email`),
+  UNIQUE KEY `username_7` (`username`),
+  UNIQUE KEY `email_7` (`email`),
+  UNIQUE KEY `username_8` (`username`),
+  UNIQUE KEY `email_8` (`email`),
+  UNIQUE KEY `username_9` (`username`),
+  UNIQUE KEY `email_9` (`email`),
+  UNIQUE KEY `username_10` (`username`),
+  UNIQUE KEY `email_10` (`email`),
+  UNIQUE KEY `username_11` (`username`),
+  UNIQUE KEY `email_11` (`email`),
+  UNIQUE KEY `username_12` (`username`),
+  UNIQUE KEY `email_12` (`email`),
+  UNIQUE KEY `username_13` (`username`),
+  UNIQUE KEY `email_13` (`email`),
+  UNIQUE KEY `username_14` (`username`),
+  UNIQUE KEY `email_14` (`email`),
+  UNIQUE KEY `username_15` (`username`),
+  UNIQUE KEY `email_15` (`email`),
+  UNIQUE KEY `username_16` (`username`),
+  UNIQUE KEY `email_16` (`email`),
+  UNIQUE KEY `username_17` (`username`),
+  UNIQUE KEY `email_17` (`email`),
+  UNIQUE KEY `username_18` (`username`),
+  UNIQUE KEY `email_18` (`email`),
+  UNIQUE KEY `username_19` (`username`),
+  UNIQUE KEY `email_19` (`email`),
+  UNIQUE KEY `username_20` (`username`),
+  UNIQUE KEY `email_20` (`email`),
+  UNIQUE KEY `username_21` (`username`),
+  UNIQUE KEY `email_21` (`email`),
+  UNIQUE KEY `username_22` (`username`),
+  UNIQUE KEY `email_22` (`email`),
+  UNIQUE KEY `username_23` (`username`),
+  UNIQUE KEY `email_23` (`email`),
+  UNIQUE KEY `username_24` (`username`),
+  UNIQUE KEY `email_24` (`email`),
+  UNIQUE KEY `username_25` (`username`),
+  UNIQUE KEY `email_25` (`email`),
+  UNIQUE KEY `username_26` (`username`),
+  UNIQUE KEY `email_26` (`email`),
+  UNIQUE KEY `username_27` (`username`),
+  UNIQUE KEY `email_27` (`email`),
+  UNIQUE KEY `username_28` (`username`),
+  UNIQUE KEY `email_28` (`email`),
+  UNIQUE KEY `username_29` (`username`),
+  UNIQUE KEY `email_29` (`email`),
+  UNIQUE KEY `username_30` (`username`),
+  UNIQUE KEY `email_30` (`email`),
+  UNIQUE KEY `username_31` (`username`),
+  UNIQUE KEY `email_31` (`email`),
+  UNIQUE KEY `username_32` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Test','jean@mail.fr','$2b$10$84WPHWsji2/NLey2c73JDeHwpR1a16O41n1N5eYt9A58JCktlkPJK','http://localhost:3000/images/profile/cat1644228440357.jpg',1,'2022-01-20 12:48:00','2022-02-07 10:07:20'),(5,'Jean','jean@jean.fr','$2b$10$Y27xfN2bt5Rd6hh/Yhs8Te8ek13um1achuubwbaRF4iR.HlZBAQLW','http://localhost:3000/images/profile/Luna.png',0,'2022-01-26 16:28:25','2022-01-26 16:28:25'),(8,'Sophie','sophie@mail.fr','$2b$10$Dae3LTwAL5NgHsPVbX.2/eLecCK4YHR2RaCVYo/2OLDSGq5QVyTH.','http://localhost:3000/images/profile/NASA1644157529363.jpg',0,'2022-01-27 09:01:04','2022-02-06 14:25:29');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-07 11:30:55
